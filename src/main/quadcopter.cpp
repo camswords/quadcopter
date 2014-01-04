d1bf6fa3541ade6a3500d25ebfe75f173c971694
Cam Swords <cam.swords@gmail.com>
2014-01-04 22:33:24 +1100

    Upload the code written at the last meet up to the quadcopter file.

--- src/main/quadcopter.cpp
+++ src/main/quadcopter.cpp
@@ -1,21 +1,39 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-int main(void)
-{
+int main(void) {
     init();
 
     #if defined(USBCON)
         USBDevice.attach();
     #endif
+
+    Servo motorA;
+    Servo motorB;
+    Servo motorC;
+    Servo motorD;
     
-    pinMode(13, OUTPUT);
+    // Input from the RC receiver (throttle channel)
+    pinMode(A0, INPUT);
+    
+    // Output throttle values for each of the motors
+    motorA.attach(A3);
+    motorB.attach(A4);
+    motorC.attach(A5);
+    motorD.attach(A6);
     
     for (;;) {
-        digitalWrite(13, HIGH);
-        delay(1000);
-        digitalWrite(13, LOW);
-        delay(200);
+        // Read the pulse width from the RC receiver
+        int throttle = pulseIn(A0, HIGH, 25000); 
+
+        // Write the same throttle to all of the motors
+        motorA.writeMicroseconds(throttle);
+        motorB.writeMicroseconds(throttle);
+        motorC.writeMicroseconds(throttle);
+        motorD.writeMicroseconds(throttle);
+        
+        // Wait a bit, just for kicks
+        delay(100);
 
         if (serialEventRun) serialEventRun();
     }
