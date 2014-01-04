8c2b7ee2772df2b22d21c4498726294aece37365
Cam Swords <cam.swords@gmail.com>
2014-01-04 21:58:28 +1100

    The upload script will halt whenever it finds an error.

--- upload.sh
+++ upload.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
+function fail_if_error {
+    if [ $? -ne 0 ]; then
+        echo
+        echo $1
+        echo
+        exit 1    
+    fi
+}
+
 # Run the scons compile script
 ARDUINO_BOARD=yun ARSCONS_TARGET=quadcopter scons -f src/build/SConstruct 
+fail_if_error "Compilation failed, aborting."
 
 # deploy the code using the Arduino Yun linino
-scp scripts/deploy.sh root@tw.quadcopter:~
+scp src/build/deploy.sh root@tw.quadcopter:~
+fail_if_error "Failed to copy deploy script to Arduino, aborting."
+
 scp build/quadcopter.hex root@tw.quadcopter:~
+fail_if_error "Failed to copy quadcopter binary to Arduino, aborting."
+
 ssh root@tw.quadcopter 'chmod 755 ~/deploy.sh; ~/deploy.sh'
+fail_if_error "Failed to deploy quadcopter binary on Arduino, aborting."
 
 # remove all .o files that were (annoyingly) compiled into the src directory
 rm -rf src/main/*.o
