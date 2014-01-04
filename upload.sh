88f6ad861e1e58d5170012b3e73b7bb07d39c7c3
Cam Swords <cam.swords@gmail.com>
2014-01-04 21:45:13 +1100

    the artifact is created in the build directory so that we don’t pollute the root directory.

--- upload.sh
+++ upload.sh
@@ -5,7 +5,7 @@ ARDUINO_BOARD=yun ARSCONS_TARGET=quadcopter scons -f src/build/SConstruct
 
 # deploy the code using the Arduino Yun linino
 scp scripts/deploy.sh root@tw.quadcopter:~
-scp quadcopter.hex root@tw.quadcopter:~
+scp build/quadcopter.hex root@tw.quadcopter:~
 ssh root@tw.quadcopter 'chmod 755 ~/deploy.sh; ~/deploy.sh'
 
 # remove all .o files that were (annoyingly) compiled into the src directory
