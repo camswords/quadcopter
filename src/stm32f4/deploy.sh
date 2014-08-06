5038915592d49639914db4f12eaa6c6dda545c76
Cam Swords <cam.swords@gmail.com>
2014-08-06 20:28:39 +1000

    moved command line options to new line for readability

--- src/stm32f4/deploy.sh
+++ src/stm32f4/deploy.sh
@@ -4,4 +4,10 @@ set -e
 make clean all
 
 # upload the file to the usb connected board. Please ensure your board is setup in bootloader mode
-dfu-util --device 0483:df11 --cfg 1 --intf 0 --alt 0 --dfuse-address 0x08000000 --download build/quadcopter.bin
+dfu-util \
+    --device 0483:df11 \
+    --cfg 1 \
+    --intf 0 \
+    --alt 0 \
+    --dfuse-address 0x08000000 \
+    --download build/quadcopter.bin
