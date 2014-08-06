dffc2558556487a258431fc0f5f79e08c685ff2f
Cam Swords <cam.swords@gmail.com>
2014-08-06 21:32:36 +1000

    one of the lights blinks through the sole use of a timer

--- src/stm32f4/src/main.c
+++ src/stm32f4/src/main.c
@@ -1,12 +1,17 @@
 #include <stm32f4xx.h>
 #include <stm32f4xx_gpio.h>
 
-void InitializeLEDs()
+void Delay(__IO uint32_t nCount) {
+  while(nCount--) {
+  }
+}
+
+void InitialiseLEDs()
 {
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
     GPIO_InitTypeDef gpioStructure;
-    gpioStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+    gpioStructure.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
     gpioStructure.GPIO_Mode = GPIO_Mode_OUT;
     gpioStructure.GPIO_Speed = GPIO_Speed_100MHz;
     gpioStructure.GPIO_OType = GPIO_OType_PP;
@@ -14,8 +19,46 @@ void InitializeLEDs()
     GPIO_Init(GPIOD, &gpioStructure);
 }
 
-int main(void) {
-  InitializeLEDs();
+void InitialiseTimer()
+{
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
 
-  GPIOD->BSRRL = 0xF000;
+    TIM_TimeBaseInitTypeDef timerInitStructure;
+    timerInitStructure.TIM_Prescaler = 40000;
+    timerInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    timerInitStructure.TIM_Period = 500;
+    timerInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    timerInitStructure.TIM_RepetitionCounter = 0;
+    TIM_TimeBaseInit(TIM4, &timerInitStructure);
+    TIM_Cmd(TIM4, ENABLE);
+}
+
+
+void InitialisePWMChannel()
+{
+    GPIO_InitTypeDef gpioStructure;
+    gpioStructure.GPIO_Pin = GPIO_Pin_12;
+    gpioStructure.GPIO_Mode = GPIO_Mode_AF;
+    gpioStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOD, &gpioStructure);
+
+    TIM_OCInitTypeDef outputChannelInit = {0,};
+    outputChannelInit.TIM_OCMode = TIM_OCMode_PWM1;
+    outputChannelInit.TIM_Pulse = 400;
+    outputChannelInit.TIM_OutputState = TIM_OutputState_Enable;
+    outputChannelInit.TIM_OCPolarity = TIM_OCPolarity_High;
+
+    TIM_OC1Init(TIM4, &outputChannelInit);
+    TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
+
+    GPIO_PinAFConfig(GPIOD, GPIO_PinSource12, GPIO_AF_TIM4);
+}
+
+
+int main(void) {
+  InitialiseLEDs();
+  InitialiseTimer();
+  InitialisePWMChannel();
+
+  GPIOD->BSRRL = 0xE000;
 }
