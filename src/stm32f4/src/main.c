0558f08315d10d859c404eb49178bba1b8063291
Cam Swords <cam.swords@gmail.com>
2014-08-08 23:23:53 +1000

    configuring the timer for full throttle works and has been verified

--- src/stm32f4/src/main.c
+++ src/stm32f4/src/main.c
@@ -56,7 +56,7 @@ void InitialisePWMChannel()
     TIM_OCInitTypeDef outputChannelInit = {0,};
     outputChannelInit.TIM_OCMode = TIM_OCMode_PWM1;
 
-    // low throttle
+    // high throttle
     outputChannelInit.TIM_Pulse = 2000;
     outputChannelInit.TIM_OutputState = TIM_OutputState_Enable;
     outputChannelInit.TIM_OCPolarity = TIM_OCPolarity_High;
