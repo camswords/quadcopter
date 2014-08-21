80bc770778520d34130fb02927f12f3295209ac7
Cam Swords <cam.swords@gmail.com>
2014-08-21 21:42:49 +1000

    added a test harness to verify whether the pwm input is correct or not

--- src/stm32f4/src/main.c
+++ src/stm32f4/src/main.c
@@ -6,7 +6,12 @@
 #include <on_board_leds.h>
 #include <pwm_input.h>
 
+
+
 int main(void) {
+  uint8_t toggle = 0;
+
+  EnableTiming();
   InitialiseLeds();
   InitialiseSysTick();
   InitialisePWM();
@@ -17,12 +22,33 @@ int main(void) {
   DutyCycle dutyCycle3 = InitialisePWMChannel(GPIOB, GPIO_Pin_0, GPIO_PinSource0, 3);
   DutyCycle dutyCycle4 = InitialisePWMChannel(GPIOB, GPIO_Pin_1, GPIO_PinSource1, 4);
 
-  dutyCycle1.update(1000);
+  dutyCycle1.update(1100);
   dutyCycle2.update(1200);
   dutyCycle3.update(1800);
   // dutyCycle4 should be 2000
 
-  TurnOn(BLUE_LED);
+  while(1) {
+    TurnOff(RED_LED | ORANGE_LED | YELLOW_LED | BLUE_LED);
 
-  while(1);
+    if (pwmInput.dutyCycle <= 0 || pwmInput.dutyCycle >= 100) {
+      if (++toggle % 2 == 0) {
+        TurnOn(RED_LED);
+      }
+    } else {
+      if (pwmInput.dutyCycle > 0 && pwmInput.dutyCycle < 19) {
+        TurnOn(BLUE_LED);
+      } else if (pwmInput.dutyCycle >= 19 && pwmInput.dutyCycle < 79) {
+        TurnOn(ORANGE_LED);
+      } else if (pwmInput.dutyCycle >= 79 && pwmInput.dutyCycle < 99) {
+        TurnOn(YELLOW_LED);
+      } else if (pwmInput.dutyCycle >= 79 && pwmInput.dutyCycle < 99) {
+        TurnOn(YELLOW_LED | ORANGE_LED | BLUE_LED);
+      } else {
+        TurnOn(RED_LED | BLUE_LED);
+      }
+    }
+
+    // wait a second!
+    TimingDelay(168000000);
+  }
 }
