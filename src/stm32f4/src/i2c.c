ceb4950f2c0a28b70a8c9e40391b6ff0420f8155
Cam Swords <cam.swords@gmail.com>
2014-09-01 22:16:08 +1000

    initialise the gyroscope using i2c

--- src/stm32f4/src/i2c.c
+++ src/stm32f4/src/i2c.c
@@ -1,6 +1,31 @@
 #include <i2c.h>
 #include <stm32f4xx_i2c.h>
 
+/* Note: could PEC positioning help ensure correctness? */
+
+/* Events:
+	I2C_EVENT_SLAVE_TRANSMITTER_ADDRESS_MATCHED: EV1
+	I2C_EVENT_SLAVE_RECEIVER_ADDRESS_MATCHED: EV1
+	I2C_EVENT_SLAVE_TRANSMITTER_SECONDADDRESS_MATCHED: EV1
+	I2C_EVENT_SLAVE_RECEIVER_SECONDADDRESS_MATCHED: EV1
+	I2C_EVENT_SLAVE_GENERALCALLADDRESS_MATCHED: EV1
+	I2C_EVENT_SLAVE_BYTE_RECEIVED: EV2
+	(I2C_EVENT_SLAVE_BYTE_RECEIVED | I2C_FLAG_DUALF): EV2
+	(I2C_EVENT_SLAVE_BYTE_RECEIVED | I2C_FLAG_GENCALL): EV2
+	I2C_EVENT_SLAVE_BYTE_TRANSMITTED: EV3
+	(I2C_EVENT_SLAVE_BYTE_TRANSMITTED | I2C_FLAG_DUALF): EV3
+	(I2C_EVENT_SLAVE_BYTE_TRANSMITTED | I2C_FLAG_GENCALL): EV3
+	I2C_EVENT_SLAVE_ACK_FAILURE: EV3_2
+	I2C_EVENT_SLAVE_STOP_DETECTED: EV4
+	I2C_EVENT_MASTER_MODE_SELECT: EV5
+	I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED: EV6
+	I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED: EV6
+	I2C_EVENT_MASTER_BYTE_RECEIVED: EV7
+	I2C_EVENT_MASTER_BYTE_TRANSMITTING: EV8
+	I2C_EVENT_MASTER_BYTE_TRANSMITTED: EV8_2
+	I2C_EVENT_MASTER_MODE_ADDRESS10: EV9
+*/
+
 void InitialiseI2C() {
 
 	/* enable the clock to i2c1 */
@@ -45,7 +70,7 @@ void InitialiseI2C() {
 	I2C_InitStruct.I2C_OwnAddress1 = 0x00;
 
 	/* Disable this for now, though we might need to turn on later */
-	I2C_InitStruct.I2C_Ack = I2C_Ack_Disable;
+	I2C_InitStruct.I2C_Ack = I2C_Ack_Enable;
 
 	/* 7 bit acknowledge addresses, hmm strange. */
 	I2C_InitStruct.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
@@ -53,5 +78,65 @@ void InitialiseI2C() {
 
 	/* Turn it on! */
 	I2C_Cmd(I2C1, ENABLE);
+};
+
+void SendStart() {
+	/* Begin comms! */
+	I2C_GenerateSTART(I2C1, ENABLE);
+
+	/* Start condition has been correctly released on the bus (will fail if another device attempts to communicate and bus not free) */
+	/* hmmm. I don't like while loops that never stop. */
+	while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));
+}
+
+void SendAddress(uint8_t address, uint8_t direction) {
+	/* send the address */
+	I2C_Send7bitAddress(I2C1, address, direction);
+
+	/* Wait for peripheral to acknowledge (own up) to the sent address */
+	while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+}
+
+void SendData(uint8_t data) {
+	/* send the data */
+	I2C_SendData(I2C1, data);
+
+	/* wait for confirmation */
+	/* Testing on this event over I2C_EVENT_MASTER_BYTE_TRANSMITTING is more reliable, and slower. */
+	while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+}
+
+void InitialiseGyroscope() {
+	SendStart();
+
+	/* Talk to the Gyro */
+	SendAddress(0xD0, I2C_Direction_Transmitter);
+
+	/* Reset the Gyro */
+	SendData(0x3E);
+	SendData(0x80);
+
+	/* Setup:
+	 * the full scale range of the gyro should be +/-2000 degrees / second
+	 * digital low pass filter bandwidth is 42Hz, internal sample rate is 1kHz.
+	 * Note: we could adjust the low pass filter in future to see the impact.
+	 */
+	SendData(0x16);
+	SendData(0x1B);
+
+	/* Set the sample rate
+	 * Sample rate = internal sample rate / (divider + 1)
+	 * Setting divider to 4 to give a sample rate of 200Hz.
+	 * The gyro values will update every 5ms.
+	 */
+	SendData(0x15);
+	SendData(0x04);
+
+	/* Set the clock source to PLL with Z Gyro as the reference.
+	 * This should be more stable / accurate than an internal oscillator (which would be greatly affected by temperature)
+	 * Probably not as good as an external oscillator though.
+	 */
+	SendData(0x3E);
+	SendData(0x03);
 };
 
